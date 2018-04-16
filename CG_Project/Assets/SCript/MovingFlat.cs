d0aa377c9042e76e9020466bfea1da51c5878c6d
levanmanhbka <levanmanhbka@gmail.com>
2018-04-16 08:16:56 +0700

    Video 5 Di chuyen bac them FallingFlat and MovingFlat

---
+++ CG_Project/Assets/SCript/MovingFlat.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingFlat : MonoBehaviour {
+    public float speed = 0.05f, changeDirection = -1;
+    Vector3 Move;
+
+	// Use this for initialization
+	void Start () {
+        Move = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move.x += speed;
+        this.transform.position = Move;
+	}
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("Ground"))
+        {
+            Debug.Log("MovingFlat va cham");
+            speed *= changeDirection;
+        }
+    }
+}
