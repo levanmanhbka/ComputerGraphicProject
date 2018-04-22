52df7cc0d8ac032787d5c784c04859d4f50d8b91
levanmanhbka <levanmanhbka@gmail.com>
2018-04-23 02:46:16 +0700

    Video 12 Tao AI cho dich

---
+++ CG_Project/Assets/SCript/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float lifetime = 2;
+
+
+    void Start()
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Bullet OnTriggerEnter2D");
+        if (col.isTrigger == false)
+        {
+            if (col.CompareTag("Player"))
+            {
+                col.SendMessageUpwards("Damage", 1);
+                Debug.Log("Bullet col.SendMessageUpwards ");
+            }
+            Destroy(gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+            Destroy(gameObject);
+    }
+}
